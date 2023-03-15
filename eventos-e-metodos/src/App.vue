138233905e7d63d4718afdcfdc2922788300ba77
Mariana Garcia <maridgarcia045@gmail.com>
2023-03-15 15:57:31 -0300

    curso vue - eventos e metodos

---
+++ eventos-e-metodos/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Minha lista de tarefas</h1>
+    <input type="text" placeholder="insira nova tarefa" v-focus="focus">
+    <ul>
+      <li 
+        v-for="task, index in tasks"
+        :key="`${index}`"
+      >
+        {{ task.name }}
+        <button
+          @click="remove(task)"
+        >&times;</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const focus = {
+    inserted: (el) => {
+      el.focus()
+    }
+}
+export default {
+  directives: {
+    focus,
+  },
+  data: () => {
+    return {
+      tasks: [{ name:'Fazer curso', isDone: false }],
+      focus
+    }
+  },
+  methods: {
+    remove(task) {
+      this.tasks = this.tasks.filter((t) => t.name !== task.name)
+    }
+  }
+}
+</script>
