7ae124a85e6942e793f592c3bb9cf4ecaa5f6448
Mariana Garcia <maridgarcia045@gmail.com>
2023-03-15 17:03:51 -0300

    eventos e metodos

--- eventos-e-metodos/src/App.vue
+++ eventos-e-metodos/src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <h1>Minha lista de tarefas</h1>
-    <input type="text" placeholder="insira nova tarefa" v-focus="focus">
+    <input
+      type="text"
+      placeholder="insira nova tarefa"
+      v-focus
+      v-model="currentTask"
+      @keyup.enter="add">
     <ul>
       <li 
         v-for="task, index in tasks"
         :key="`${index}`"
+        @dblclick="complete(task)"
+        class="task-item"
+        :class="{
+          'completed': task.isDone
+        }"
       >
         {{ task.name }}
         <button
@@ -26,16 +36,45 @@ export default {
   directives: {
     focus,
   },
+  mounted () {
+    console.log("foi montado")
+  },
   data: () => {
     return {
+      currentTask: '',
       tasks: [{ name:'Fazer curso', isDone: false }],
       focus
     }
   },
   methods: {
+    add() {
+      this.tasks.push({
+        name: this.currentTask,
+        isDone: false
+      })
+      this.currentTask = ''
+    },
     remove(task) {
       this.tasks = this.tasks.filter((t) => t.name !== task.name)
-    }
+    },
+    complete(task) {
+      this.tasks = this.tasks.map(t => {
+        if (t.name === task.name) {
+          return {...t, isDone: !t.isDone}
+        }
+        return {...t}
+      })
+    },
   }
 }
 </script>
+
+<style>
+  .completed {
+    text-decoration: line-through;
+  }
+
+  .task-item {
+    cursor: pointer;
+  }
+</style>
