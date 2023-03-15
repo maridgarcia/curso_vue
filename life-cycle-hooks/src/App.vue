6e622099150a3202e610119b56a95d7fd48268fc
Mariana Garcia <maridgarcia045@gmail.com>
2023-03-15 17:08:46 -0300

    life cycle hooks

---
+++ life-cycle-hooks/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Life cycle hooks</h1>
+</template>
+
+<script>
+// composition api
+// import { onMounted } from 'vue';
+
+// export default {
+//   setup () {
+//     onMounted(() => {
+//       console.log("mounted composition api")
+//     })
+//   }
+// }
+
+
+export default {
+  mounted () {
+    console.log("mounted")
+  }
+}
+</script>
